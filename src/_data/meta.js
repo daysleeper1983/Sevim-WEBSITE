e91d0ff7653ed057bf942198485515b873c0e8e0
daysleeper1983 <omerharun@gmail.com>
2026-08-18 18:53:52 +0300

    Delete src/_data/meta.js

--- src/_data/meta.js
+++
@@ -1,9 +0,0 @@
-// Exposes site-wide metadata (currently just the canonical site URL) as
-// Eleventy global data `meta`, used by base.njk for canonical/hreflang tags.
-// Keep this in sync with site_url in admin/config.yml.
-module.exports = function () {
-  return {
-      site_url: "https://sevimelmas.com.tr",
-        };
-        };
-        
